1ab632c7e3dd0a14a912691942d674ba2d2f317c
NovaPride <novapride27@gmail.com>
2024-05-16 18:16:58 +0300

    compl: 121-125

--- 06_121/src/App.js
+++ 06_121/src/App.js
@@ -1,14 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import {Component} from "react";
+import "./App.css";
+
+const Header = () => {
+  return <h2>Hello World</h2>
+}
+
+// const Field = () => {
+//   const holder = "Enter here";
+//   const styledField = {
+//     width: "300px"
+//   };
+//   return <input placeholder={holder} type="text" style={styledField}/>
+// }
+
+class Field extends Component {
+  render() {
+    const holder = "Enter here";
+    const styledField = {
+      width: "300px"
+    };
+    return <input placeholder={holder} type="text" style={styledField}/>
+  }
+}
+
+function Btn() {
+  //const text = "Log in";
+  const res = () => {
+    return "Log in";
+  }
+  return <button>{res()}</button>
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
 
+
+export {Header};
 export default App;
