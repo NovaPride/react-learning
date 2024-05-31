fb88e207a849d00e2275b4d7a6210509c473b970
NovaPride <novapride27@gmail.com>
2024-05-31 21:26:33 +0300

    compl: 158-161

--- 07_148/src/services/MarvelService.js
+++ 07_148/src/services/MarvelService.js
@@ -7,9 +7,7 @@ export default class MarvelService{
 
   getResource = async (url) => {
     let res = await fetch(url);
-
     if(!res.ok) throw new Error(`Couldn't fetch ${url}, status: ${res.status}`);
-    
     return await res.json();
   }
 
